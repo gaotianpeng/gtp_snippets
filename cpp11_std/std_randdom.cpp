f595d29c6052c60723c3cab239e6ae9af48d0feb
gaotianpeng <gtpsydm@gmail.com>
2020-10-24 10:01:05 +0800

    cpp11 auto feature

--- cpp11_std/std_randdom.cpp
+++ cpp11_std/std_randdom.cpp
@@ -4,12 +4,12 @@
 
 int main(int argc, char* argv[]) {
     std::default_random_engine dre;
-    std::uniform_int_distribution<int> di(10, 20);
+    std::uniform_int_distribution<int64_t> di(10, 20000000);
     
     for(int i = 0; i < 20; ++i) {
         std::cout << di(dre) << " ";
     }
-
+    std::cout << di(dre) << std::endl;
     std::cout << std::endl;
     return 0;
 }
