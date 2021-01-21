c71030da78698000832dc3f49f653a91b4c86f94
gaotianpeng <gtpsydm@gmail.com>
2021-01-21 23:17:49 +0800

    reshape

--- cpp11_std/thread/recursive_mutex.cpp
+++ cpp11_std/thread/recursive_mutex.cpp
@@ -59,11 +59,12 @@ int main(int argc, char* argv[]) {
     /*
      * 用recursive_mutex解决单一线程内多次获取同一锁的死锁问题
      */
-    ComplexRecur complex;
+    ComplexRecur complex_cur;
     cout << "ComplexRecur start ---------------------------" << endl;
-    complex.both(32, 23);
-    cout << "ComplexRecur end    ---------------------------" << endl;
+    complex_cur.both(32, 23);
+    cout << "ComplexRecur end   ---------------------------" << endl;
   }
+
   {
     /*
      * 运行起来后发生死锁
@@ -71,7 +72,7 @@ int main(int argc, char* argv[]) {
      *  但是这个互斥量已经被当前线程获取了，无法释放，这时就会发生死锁
      */
     Complex complex;
-    cout << "Complex start ---------------------------" << endl;
+    cout << "Complex start  ---------------------------" << endl;
     complex.both(32, 23);
     cout << "Complex end    ---------------------------" << endl;
   }
