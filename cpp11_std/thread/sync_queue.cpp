fbb5e8aa7c911b9b3c2f009dfa147a2843071891
gaotianpeng <gtpsydm@gmail.com>
2021-08-25 09:46:01 +0800

    code style

--- cpp11_std/thread/sync_queue.cpp
+++ cpp11_std/thread/sync_queue.cpp
@@ -16,94 +16,92 @@
 
 template <typename T>
 class SyncQueue {
- public:
-  SyncQueue(int max_size): max_size_(max_size) {}
+public:
+    SyncQueue(int max_size): max_size_(max_size) {}
 
-  void Put(const T& x) {
+void Put(const T& x) {
     std::lock_guard<std::mutex> lock(mutex_);
     while (IsFull()) {
-      std::cout << "缓冲区满了，需要等待..." << std::endl;
-      not_full_.wait(mutex_);
+    std::cout << "缓冲区满了，需要等待..." << std::endl;
+    not_full_.wait(mutex_);
     }
 
     queue_.push_back(x);
     not_full_.notify_one();
-  }
+}
 
-  void Take(T& x) {
+void Take(T& x) {
     std::lock_guard<std::mutex> lock(mutex_);
     while (IsEmpty()) {
-      std::cout << "缓冲区空了，需要等待..." << std::endl;
-      not_empty_.wait(mutex_);
+        std::cout << "缓冲区空了，需要等待..." << std::endl;
+        not_empty_.wait(mutex_);
     }
 
     x = queue_.front();
     queue_.pop_front();
     not_full_.notify_one();
-  }
+}
 
-  bool Empty() {
+bool Empty() {
     std::lock_guard<std::mutex> lock(mutex_);
     return queue_.empty();
-  }
+}
 
-  bool Full() {
+bool Full() {
     std::lock_guard<std::mutex> lock(mutex_);
     return queue_.size() == max_size_;
-  }
+}
 
-  size_t Size() const {
+size_t Size() const {
     std::lock_guard<std::mutex> lock(mutex_);
     return queue_.size_;
-  }
+}
 
-  int Count() const {
+int Count() const {
     return queue_.size();
-  }
+}
 
- private:
-  bool IsFull() const {
+private:
+bool IsFull() const {
     return queue_.size() == max_size_;
-  }
+}
 
-  bool IsEmpty() const {
+bool IsEmpty() const {
     return queue_.empty();
-  }
- private:
-  std::list<T> queue_;    // 缓冲区
-  std::mutex mutex_;      // 互斥量和条件变量结合起来使用
-  std::condition_variable_any not_empty_; // 不为空的条件亦是
-  std::condition_variable_any not_full_;  // 没有满的条件变量
-  int max_size_;          // 同步队列最大的size
+}
+
+private:
+    std::list<T> queue_;    // 缓冲区
+    std::mutex mutex_;      // 互斥量和条件变量结合起来使用
+    std::condition_variable_any not_empty_; // 不为空的条件亦是
+    std::condition_variable_any not_full_;  // 没有满的条件变量
+    int max_size_;          // 同步队列最大的size
 };
 
 
 SyncQueue<int> sync_queue(5);
 
 void PutDatas() {
-  for (int i = 0; i < 20; ++i) {
+    for (int i = 0; i < 20; ++i) {
     sync_queue.Put(888);
-  }
+    }
 }
 
 void TakeDatas() {
-  int x = 0;
-  for (int i = 0; i < 20; ++i) {
-    sync_queue.Take(x);
-    std::cout << x << std::endl;
-  }
+    int x = 0;
+    for (int i = 0; i < 20; ++i) {
+        sync_queue.Take(x);
+        std::cout << x << std::endl;
+    }
 }
 
 int main(int argc, char* argv[]) {
-  std::thread th1(PutDatas);
-  std::thread th2(TakeDatas);
+    std::thread th1(PutDatas);
+    std::thread th2(TakeDatas);
 
-  th1.join();
-  th2.join();
-  system("pause");
+    th1.join();
+    th2.join();
+    system("pause");
 
-  return 0;
+    return 0;
 }
-
-
-
--- cpp11_std/thread/timed_thread.cpp
+++ cpp11_std/thread/timed_thread.cpp
@@ -6,29 +6,29 @@ using namespace std;
 std::timed_mutex mutex_;
 
 void work() {
-  std::chrono::milliseconds timeout(100);
-  while (true) {
-    if (mutex_.try_lock_for(timeout)) {
-      std::cout << std::this_thread::get_id() << ": do work with the mutex" << std::endl;
-      std::chrono::milliseconds sleep_dur(250);
-      std::this_thread::sleep_for(sleep_dur);
-      mutex_.unlock();
-      std::this_thread::sleep_for(sleep_dur);
-    } else {
-      std::cout << std::this_thread::get_id() << ": do work without the mutex" << std::endl;
-      std::chrono::milliseconds sleep_dur(100);
-      std::this_thread::sleep_for(sleep_dur);
+    std::chrono::milliseconds timeout(100);
+    while (true) {
+        if (mutex_.try_lock_for(timeout)) {
+            std::cout << std::this_thread::get_id() << ": do work with the mutex" << std::endl;
+            std::chrono::milliseconds sleep_dur(250);
+            std::this_thread::sleep_for(sleep_dur);
+            mutex_.unlock();
+            std::this_thread::sleep_for(sleep_dur);
+        } else {
+            std::cout << std::this_thread::get_id() << ": do work without the mutex" << std::endl;
+            std::chrono::milliseconds sleep_dur(100);
+            std::this_thread::sleep_for(sleep_dur);
+        }
     }
-  }
 }
 
 int main(int argc, char* argv[]) {
-  std::thread th1(work);
-  std::thread th2(work);
-  std::thread th3(work);
+    std::thread th1(work);
+    std::thread th2(work);
+    std::thread th3(work);
 
-  th1.join();
-  th2.join();
-  th3.join();
-  return 0;
+    th1.join();
+    th2.join();
+    th3.join();
+    return 0;
 }
