f86acc331739aaca4d3a371e7e370611e1766d3d
gaotianpeng <gtpsydm@gmail.com>
2021-08-31 11:01:50 +0800

    thread join

--- cpp11_std/thread/join.cpp
+++ cpp11_std/thread/join.cpp
@@ -8,12 +8,12 @@ using namespace std;
     join()只能被调用一次
 */
 
-void print() {
+void Print() {
 	cout << "hello thread" << endl;
 }
 
-int main(int argc, char* argv[]) {
-	thread th(print);
+void JoinTest() {
+	thread th(Print);
 	if (th.joinable()) {
 		th.join();
 		cout << "first joinable!" << endl;
@@ -25,10 +25,47 @@ int main(int argc, char* argv[]) {
 	if (th.joinable()) {
 		th.join();
 		cout << "second joinable!" << endl;
-	} {
+	} 
+	else {
 		cout << "second not joinable!" << endl;
 	}
+}
 
+void DoSomething(int& i) {
+	++i;
+}
+
+struct Func {
+	int& i_;
+	Func(int& i): i_(i) {}
+
+	void operator()() {
+		for (unsigned j = 0; j < 1000000; ++j) {
+			DoSomething(i_);
+		}
+	}
+};
+
+void DoSomethingInCurrentThread() {
+}
+
+void JoinException() {
+	int local_state = 0;
+	Func my_func(local_state);
+	std::thread th(my_func);
+	try {
+		DoSomethingInCurrentThread();
+	} 
+	catch(...) {
+		th.join();
+		throw;
+	}
+	th.join();
+}
+
+int main(int argc, char* argv[]) {
+	JoinTest();
+	JoinException();
 	return 0;
 }
 
