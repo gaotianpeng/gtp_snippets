a0d931842bb234a6cd9f9f0135ff0169f43b35c4
gaotianpeng <gtpsydm@gmail.com>
2021-09-06 17:13:23 +0800

    scoped timer

--- userful_tool/scoped_timer.cpp
+++ userful_tool/scoped_timer.cpp
@@ -5,32 +5,28 @@ using namespace std;
 
 class ScopedTimer {
 public:
-	ScopedTimer(function<void(int)> cb):t0(std::chrono::high_resolution_clock::now()), cb_(cb) {}
+    ScopedTimer(const std::string& msg):t0(std::chrono::high_resolution_clock::now()), msg_(msg) {}
 
-	~ScopedTimer() {
-		auto t1 = std::chrono::high_resolution_clock::now();
-		auto nanos = std::chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
-		cb_(nanos);
-	}
+    ~ScopedTimer() {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        auto nanos = std::chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
+        cout << msg_ << ": cost time: " <<  nanos << " ms" << endl;
+    }
 
 private:
-	std::chrono::high_resolution_clock::time_point  t0;
-	function<void(int)> cb_;
+    std::chrono::high_resolution_clock::time_point  t0;
+    std::string msg_;
 };
 
-void PrintElaps(int n) {
-	std::cout << "cost time:" << n <<  " ms" << endl;
-}
-
 int main(int argc, char* argv[]) {
-	int N = 100000000;
-	long double sum = 0;
-	ScopedTimer timer(PrintElaps);
-	for (int i = 0; i < N; ++i) {
-		sum += i * 3.14159;
-	}
+    int N = 100000000;
+    long double sum = 0;
+    ScopedTimer timer("test");
+    for (int i = 0; i < N; ++i) {
+        sum += i * 3.14159;
+    }
 
-	cout << sum << endl;
+    cout << sum << endl;
 
-	return 0;
+    return 0;
 }
