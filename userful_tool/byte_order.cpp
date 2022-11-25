68af45980ed3a9c13b22ea8e1b9dab963d1a0507
gaotianpeng <gtpsydm@gmail.com>
2022-11-25 17:31:35 +0800

    usefull tools: byte order

---
+++ userful_tool/byte_order.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+using namespace std;
+
+// 判断CPU是大端还是小端
+void byteorder() {
+    union {
+        short value;
+        char union_bytes[sizeof(short)];
+    } test;
+
+    test.value = 0x0102;
+    if ((test.union_bytes[0] == 1) && (test.union_bytes[1] == 2)) {
+        printf("big endian\n");
+    } else if ((test.union_bytes[0] ==2) && (test.union_bytes[1] == 1)) {
+        printf("little endian\n");
+    } else {
+        printf("unknow...\n");
+    }
+}
+
+int main(int argc, char* argv[]) {
+    byteorder();
+    return 0;
+}
