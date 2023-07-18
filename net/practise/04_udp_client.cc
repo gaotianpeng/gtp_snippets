b2b5ac6ee981a6c52257ae8ddc4230daf215cc59
gaotianpeng <gtpsydm@gmail.com>
2023-07-18 17:30:37 +0800

    daily practise

---
+++ net/practise/04_udp_client.cc
@@ -0,0 +1,15 @@
+// c std
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+// c std linux
+#include <unistd.h>
+
+// linux
+#include <sys/socket.h>
+
+int main(int argc, char* argv[]) {
+    printf("udp client\n");
+    return 0;
+}
