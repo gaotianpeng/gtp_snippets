3b5ce5b66d80194441e8eb67e37247017bad3947
gaotianpeng <gtpsydm@gmail.com>
2023-07-19 15:06:56 +0800

    daily practise

--- net/practise/08_grace_client.cc
+++ net/practise/08_grace_client.cc
@@ -31,23 +31,22 @@ void error(int status, int err, const char* fmt, ...) {
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        error(1, 0, "usage: graceclient <IPAddress>");
-    }
+        error(1, 0, "usage: graceclient <IPaddress>");
 
+    }
     int socket_fd;
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in serv_addr;
-    bzero(&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(SERV_PORT);
-    inet_pton(AF_INET, argv[1], &serv_addr.sin_addr);
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERV_PORT);
+    inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
 
-    socklen_t serv_len = sizeof(serv_addr);
-
-    int ret = connect(socket_fd, (struct sockaddr*)&serv_addr, serv_len);
-    if (ret < 0) {
-        error(1, errno, "connect faile");
+    socklen_t server_len = sizeof(server_addr);
+    int connect_rt = connect(socket_fd, (struct sockaddr *) &server_addr, server_len);
+    if (connect_rt < 0) {
+        error(1, errno, "connect failed ");
     }
 
     char send_line[MAXLINE], recv_line[MAXLINE + 1];
@@ -55,7 +54,7 @@ int main(int argc, char* argv[]) {
 
     fd_set readmask;
     fd_set allreads;
-    
+
     FD_ZERO(&allreads);
     FD_SET(0, &allreads);
     FD_SET(socket_fd, &allreads);
@@ -64,9 +63,8 @@ int main(int argc, char* argv[]) {
         readmask = allreads;
         int rc = select(socket_fd + 1, &readmask, NULL, NULL, NULL);
         if (rc <= 0) {
-            error(1, errno, "select faield");   
+            error(1, errno, "select failed");
         }
-
         if (FD_ISSET(socket_fd, &readmask)) {
             n = read(socket_fd, recv_line, MAXLINE);
             if (n < 0) {
@@ -74,12 +72,10 @@ int main(int argc, char* argv[]) {
             } else if (n == 0) {
                 error(1, 0, "server terminated \n");
             }
- 
             recv_line[n] = 0;
             fputs(recv_line, stdout);
             fputs("\n", stdout);
         }
-
         if (FD_ISSET(0, &readmask)) {
             if (fgets(send_line, MAXLINE, stdin) != NULL) {
                 if (strncmp(send_line, "shutdown", 8) == 0) {
@@ -99,17 +95,17 @@ int main(int argc, char* argv[]) {
                     if (send_line[i - 1] == '\n') {
                         send_line[i - 1] = 0;
                     }
+
                     printf("now sending %s\n", send_line);
+                    size_t rt = write(socket_fd, send_line, strlen(send_line));
+                    if (rt < 0) {
+                        error(1, errno, "write failed ");
+                    }
+                    printf("send bytes: %zu \n", rt);
                 }
-
-                int rt = write(socket_fd, send_line, strlen(send_line));
-                if (rt < 0) {
-                    error(1, errno, "write failed");
-                }
-
-                printf("send bytes : %d \n", rt);
             }
         }
+
     }
 
     return 0;
--- net/practise/09_grace_server.cc
+++ net/practise/09_grace_server.cc
@@ -35,56 +35,55 @@ static void sig_int(int signo) {
     exit(0);
 }
 
+
 int main(int argc, char* argv[]) {
     int listenfd;
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in serv_addr;
-    bzero(&serv_addr, sizeof(serv_addr));
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(SERV_PORT);
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(SERV_PORT);
-
-    int ret = bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    if (ret < 0) {
-        error(1, errno, "bind failed");   
+    int rt1 = bind(listenfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (rt1 < 0) {
+        error(1, errno, "bind failed ");
     }
 
-    ret = listen(listenfd, 1024);
-    if (ret < 0) {
-        error(1, errno, "listen failed");
+    int rt2 = listen(listenfd, 1024);
+    if (rt2 < 0) {
+        error(1, errno, "listen failed ");
     }
 
     signal(SIGINT, sig_int);
-    signal(SIGPIPE, SIG_IGN);
+    signal(SIGPIPE, SIG_DFL);
 
     int connfd;
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
 
-    if ((connfd = accept(listenfd, (struct sockaddr*)&client_addr, &client_len)) < 0) {
-        error(1, errno, "bind failed");
+    if ((connfd = accept(listenfd, (struct sockaddr *) &client_addr, &client_len)) < 0) {
+        error(1, errno, "bind failed ");
     }
 
     char message[MAXLINE];
     count = 0;
+
     for (;;) {
-        bzero(message, sizeof(message));
         int n = read(connfd, message, MAXLINE);
         if (n < 0) {
             error(1, errno, "error read");
         } else if (n == 0) {
             error(1, 0, "client closed \n");
         }
-
         message[n] = 0;
         printf("received %d bytes: %s\n", n, message);
         count++;
-        
+
         char send_line[MAXLINE];
-        bzero(send_line, sizeof(send_line));
         sprintf(send_line, "Hi, %s", message);
+
         sleep(5);
 
         int write_nc = send(connfd, send_line, strlen(send_line), 0);
@@ -93,6 +92,4 @@ int main(int argc, char* argv[]) {
             error(1, errno, "error write");
         }
     }
-
-    return 0;
 }
